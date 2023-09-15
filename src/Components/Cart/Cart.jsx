d2501be3084986fd31a6327c951c7926ff57a59d
nasimuk47 <nasim.uk47@gmail.com>
2023-09-15 22:19:59 +0600

    showing coursename and addtotalprice

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,8 +1,78 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Cart = () => {
-    return <div>This is cart</div>;
+const Cart = ({ selectedCourses }) => {
+    const [totalCreditHours, setTotalCreditHours] = useState(0);
+
+    const calculateTotalPrice = () => {
+        // Calculate the total price from the selected courses
+        return selectedCourses.reduce(
+            (total, course) => total + course.price,
+            0
+        );
+    };
+
+    useEffect(() => {
+        // Calculate the total credit hours from the selected courses
+        const calculatedTotalCreditHours = selectedCourses.reduce(
+            (total, course) => total + parseInt(course.credit),
+            0
+        );
+
+        setTotalCreditHours(calculatedTotalCreditHours);
+    }, [selectedCourses]);
+
+    const addToCart = (course) => {
+        // Calculate the total credit hours after adding the new course
+        const updatedTotalCreditHours =
+            totalCreditHours + parseInt(course.credit);
+
+        if (updatedTotalCreditHours > 20) {
+            // If total credit hours exceed 20, show a toast message and do not add the course
+            toast.error("Total credit hours cannot exceed 20 hours.");
+        } else {
+            // Otherwise, add the course to the selected courses
+            selectedCourses.push(course);
+        }
+    };
+
+    // Ensure that "Total Credit Hour" does not exceed 20 hours
+    const displayTotalCreditHours =
+        totalCreditHours > 20 ? 20 : totalCreditHours;
+
+    return (
+        <div>
+            <div className="text-xl font-medium text-blue-600 mb-3">
+                Credit Hour Remaining: {20 - displayTotalCreditHours} hr
+            </div>
+            <hr />
+
+            <div className="text-xl font-bold mt-7">Selected Courses:</div>
+
+            <ol>
+                {selectedCourses.map((course, index) => (
+                    <li key={index} className="text-xl">
+                        {`${index + 1} ${course.name}`}
+                    </li>
+                ))}
+            </ol>
+
+            <div className="text-xl font-semibold">
+                Total Credit Hour: {displayTotalCreditHours} hr
+            </div>
+
+            <div className="text-xl font-medium">
+                Total Price: {calculateTotalPrice()} <span>USD</span>
+            </div>
+            <hr />
+
+            {/* Add the ToastContainer to your component */}
+            <ToastContainer />
+        </div>
+    );
 };
 
 export default Cart;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,11 +6,24 @@ import Cart from "../Cart/Cart";
 
 const Home = () => {
     const [allcard, setAllCard] = useState([]);
+    const [selectedCourses, setSelectedCourses] = useState([]);
+
     useEffect(() => {
         fetch("./data.json")
             .then((res) => res.json())
             .then((data) => setAllCard(data));
     }, []);
+
+    const handleSelectCourse = (course) => {
+        // Check if the course is already selected to avoid duplicates
+        if (
+            !selectedCourses.find(
+                (selectedCourse) => selectedCourse.id === course.id
+            )
+        ) {
+            setSelectedCourses([...selectedCourses, course]);
+        }
+    };
 
     return (
         <div className="">
@@ -18,36 +31,40 @@ const Home = () => {
                 Course Registration
             </h2>
 
-            <div className="flex ">
-                <div className="w-9/12 grid  grid-cols-3 ">
+            <div className="flex">
+                <div className="w-9/12 grid grid-cols-3">
                     {allcard.map((singlecard) => (
-                        <div key={singlecard.id} className=" ">
-                            <div className="card w-80  bg-base-100 shadow-xl rounded-xl mt-10 ">
+                        <div key={singlecard.id} className="">
+                            <div className="card w-80 bg-base-100 shadow-xl rounded-xl mt-10">
                                 <figure className="px-10 pt-10">
                                     <img
                                         src={singlecard.img}
-                                        alt="Shoes"
+                                        alt={singlecard.name}
                                         className="rounded-xl"
                                     />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title text-2xl font-bold mt-2">
+                                    <h2 className="card-title text-xl font-bold mt-2">
                                         {singlecard.name}
                                     </h2>
                                     <p className="mt-2">{singlecard.title}</p>
 
                                     <div className="flex gap-2 justify-center mt-3">
                                         <p>
-                                            <span>$</span> Price : 15000
+                                            <span>$</span> Price: 15000
                                         </p>
 
                                         <div>icon</div>
 
-                                        <p>{singlecard.credit}</p>
+                                        <p>Credit: {singlecard.credit} hr</p>
                                     </div>
 
                                     <div className="">
-                                        <button className="bg-sky-300 w-56 rounded-md p-2 mt-3 mb-3 ">
+                                        <button
+                                            className="bg-sky-300 w-56 rounded-md p-2 mt-3 mb-3"
+                                            onClick={() =>
+                                                handleSelectCourse(singlecard)
+                                            }>
                                             Select
                                         </button>
                                     </div>
@@ -57,7 +74,7 @@ const Home = () => {
                     ))}
                 </div>
                 <div className="bg-base-100 w-72 ml-5 ">
-                    <Cart></Cart>
+                    <Cart selectedCourses={selectedCourses} />
                 </div>
             </div>
         </div>
